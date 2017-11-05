70a0ed292015854f85b7f4a3f57e1812c6bd9b5c
codermaz <yilmazadem@gmail.com>
2017-11-05 19:53:56 +0100

    Seeds OK

--- Controlling Keyboard/ViewController.swift
+++ Controlling Keyboard/ViewController.swift
@@ -5,21 +5,45 @@
 //  Created by MacMini on 05.11.17.
 //  Copyright © 2017 MacMini. All rights reserved.
 //
+/*
+ 
+    A>> Main.storyboard
+    1. TextField auf den Stroryboard
+    2.1. Ctrl + mouse Links >> "Gelb-view controller"
+    2.2. Delegate auswählen
+ 
+    B>> ViewController.swift
+    1.1. override func touchesBegan
+    1.2. self.view.endEditing(true)
+    2.1. als erwerbClasse "UITextFieldDelegate" hinzufügen
+    2.2. func textFieldShouldReturn
+            textField.resignFirstResponder()
+            true
+*/
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // keyboard wegschicken when ausser TextField drücken
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // keyboard wegschicken when Return pressed
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
